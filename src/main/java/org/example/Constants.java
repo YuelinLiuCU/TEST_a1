495a43094bcb4a7f7c4eaa136346da3038a1c2fa
Carenliu <yuelinliu@cmail.carleton.ca>
2022-10-29 14:06:12 -0400

    added test

---
+++ src/main/java/org/example/Constants.java
@@ -0,0 +1,37 @@
+package org.example;
+
+public class Constants {
+    public static final int GAME_SERVER_PORT_NUMBER = 5001;
+    public static final int PLAYER_NUM = 3;
+    public static final int DICE_NUM = 8;
+    public static final int DIE_FACE_NUM = 6;
+
+    // Dice name
+    public static final String MONKEY = "Monkey";
+    public static final String PARROT= "Parrot";
+    public static final String SWORD = "Sword";
+    public static final String SKULL = "Skull";
+    public static final String COIN = "Coin";
+    public static final String DIAMOND = "Diamond";
+
+    // Fortune card name
+    public static final String CHEST = "Chest";
+    public static final String SORCERESS = "Sorceress";
+    public static final String CAPTAIN = "Captain";
+    public static final String MONKEY_AND_PARROT = "Monkey & Parrot";
+    public static final String DIAMOND_CARD = "Diamond";
+    public static final String COIN_CARD = "Coin";
+    public static final String ONE_SKULL = "Skull x 1";
+    public static final String TWO_SKULLS = "Skull x 2";
+    public static final String TWO_SWORDS = "Swords x 2(300 bonus)";
+    public static final String THREE_SWORDS = "Swords x 3(500 bonus)";
+    public static final String FOUR_SWORDS = "Swords x 4(1000 bonus)";
+
+    // Scoring
+    public static final int THREE_OF_A_KIND_POINTS = 100;
+    public static final int FOUR_OF_A_KIND_POINTS = 200;
+    public static final int FIVE_OF_A_KIND_POINTS = 500;
+    public static final int SIX_OF_A_KIND_POINTS = 1000;
+    public static final int SEVEN_OF_A_KIND_POINTS = 2000;
+    public static final int EIGHT_OF_A_KIND_POINTS = 4000;
+}
---
+++ src/test/java/unitTest.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+import junit.framework.TestCase;
+
+public class unitTest extends TestCase{
+    public void testCombineDice() {
+        Dice dice = new Dice();
+    }
+}
